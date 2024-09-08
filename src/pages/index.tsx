91d81404c3de708283ce27e4fa40c5832e5d3cf1
Madhoolika Nagesh <madhoolikanagesh@gmail.com>
2024-09-08 10:16:54 +0530

    .eslintric update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Head from "next/head";
 import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs';
 import StudentForm from '../components/StudentForm';
